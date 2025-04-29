08e57c3a7bc521fc845d0edc37f1e2a8bb311ae8
BrandenXia <xxtbranden@outlook.com>
2025-04-29 09:36:33 -0700

    good

--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ import subprocess
 
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).parent.parent
+FILE_ROOT = Path(__file__).parent
+ROOT_DIR = FILE_ROOT.parent
+DATA_DIR = FILE_ROOT / "data"
 GAF_DIR = ROOT_DIR / "gaf"
 GFF_DIR = ROOT_DIR / "gff"
 GTF_DIR = ROOT_DIR / "gtf"
